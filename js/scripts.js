786fd9bfa8a76c97284190d854bb4c9a6f7c42b9
maria98x <53050052+maria98x@users.noreply.github.com>
2021-12-07 16:55:37 +0300

    v2

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@
     
     // Toggle the side navigation  
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
- 
+    const toggleDiv = document.querySelector(".sideNav")
+    sidebarToggle.addEventListener("click",function(){
+        toggleDiv.classList.toggle("toggleNav");
+
+    })
+
+   
